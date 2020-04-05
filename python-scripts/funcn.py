d22cb4e04e7b8ff54a7baf2a615bc170fe4b926d
DavZhangDev <david.zhangshengyang@gmail.com>
2020-04-05 18:29:04 +0800

    Modify main.py

--- python-scripts/funcn.py
+++ python-scripts/funcn.py
@@ -1,18 +1,29 @@
 import pyautogui
 import sys
 import time
+import math
 SCREENHEIGHT = pyautogui.size()[1]
 SCREENWIDTH = pyautogui.size()[0]
+
+def navigatetourl(URL, MORW):
+    if MORW.lower() == "m":
+        pyautogui.hotkey("command", "t")
+    else:
+        pyautogui.hotkey("ctrl", "t")
+    time.sleep(1)
+    pyautogui.write(URL)
+    pyautogui.hotkey("enter")
+
 def inputunpw(USERNAME, PASSWORD):
-    pyautogui.moveTo((1156/1440)*SCREENWIDTH, (431/900)*SCREENHEIGHT)
+    pyautogui.moveTo(math.floor((1156/1440)*SCREENWIDTH), math.floor((431/900)*SCREENHEIGHT))
     pyautogui.click()
     pyautogui.hotkey("ctrl", "shift", "a", "backspace")
     pyautogui.write(USERNAME)
     time.sleep(0.1)
-    pyautogui.click((10/1440)*SCREENWIDTH, (450/900)*SCREENHEIGHT)
+    pyautogui.click(math.floor((10/1440)*SCREENWIDTH), math.floor((450/900)*SCREENHEIGHT))
     time.sleep(0.1)
-    pyautogui.click((1156/1440)*SCREENWIDTH, (471/900)*SCREENHEIGHT)
+    pyautogui.click(math.floor((1156/1440)*SCREENWIDTH), math.floor((471/900)*SCREENHEIGHT))
     pyautogui.hotkey("ctrl", "shift", "a", "backspace")
     pyautogui.write(PASSWORD)
     time.sleep(0.1)
-    pyautogui.click((1032/1440)*SCREENWIDTH, (540/900)*SCREENHEIGHT)
+    pyautogui.click(math.floor((1032/1440)*SCREENWIDTH), math.floor((540/900)*SCREENHEIGHT))
--- python-scripts/main.py
+++ python-scripts/main.py
@@ -1,22 +1,29 @@
-import time
-import pyautogui
-import sys
-from funcn import *
-SCREENHEIGHT = pyautogui.size()[1]
-SCREENWIDTH = pyautogui.size()[0]
-USERNAME = input("USERNAME: ")
-PASSWORD = input("PASSWORD: ")
-LOGINTIME = input("LOGIN TIME (24H FORMAT): ")
-LOGINTIME = [int(str(LOGINTIME)[0] + str(LOGINTIME)[1]), int(str(LOGINTIME)[2] + str(LOGINTIME)[3])]
-print(LOGINTIME)
-print("Your screen dimensions are: " + str(SCREENWIDTH) + " * " + str(SCREENHEIGHT))
-logged = True
 try:
+    import time
+    import pyautogui
+    import sys
+    import math
+    from funcn import *
+    SCREENHEIGHT = pyautogui.size()[1]
+    SCREENWIDTH = pyautogui.size()[0]
+    USERNAME = input("USERNAME: ")
+    PASSWORD = input("PASSWORD: ")
+    LOGINTIME = input("LOGIN TIME (24H FORMAT): ")
+    LOGINTIME = [int(str(LOGINTIME)[0] + str(LOGINTIME)[1]), int(str(LOGINTIME)[2] + str(LOGINTIME)[3])]
+    URL = input("INPUT URL: ")
+    MORW = input("MAC OR WINDOWS (M/W): ")
+    print("Your screen dimensions are: " + str(SCREENWIDTH) + " * " + str(SCREENHEIGHT))
+    logged = True
     while logged:
         time.sleep(5)
         t = time.localtime()
         if ([t[3], t[4]] == LOGINTIME):
+            navigatetourl(URL, MORW)
+            time.sleep(10)
             inputunpw(USERNAME, PASSWORD)
             logged = False
+            print("Logged in at " + str(t[3]) + ":" + str(t[4]))
 except KeyboardInterrupt:
-    print('\n')
+    print("\nProgram stopped.")
+except:
+    print("\nError encountered, please rerun program!")
